0ae68f7fe1268063480a32f7c9f762ba94519da6
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-05 14:47:03 +0200

    refactor: change main to be a server component

--- app/page.js
+++ app/page.js
@@ -1,24 +1,31 @@
-'use client'
-import { fetchData } from '@/api/api'
-import { useEffect, useState } from 'react'
-export default function Home() {
-  const [data, setData] = useState([])
+import FAQ from '@/components/FAQ'
+export const dynamic = 'force-dynamic'
 
-  useEffect(() => {
-    async function fetchDataFromApi() {
-      try {
-        const apiData = await fetchData()
-        setData(apiData)
-      } catch (e) {
-        //handle error
-      }
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      'https://pre-prod.harbour.space/api/v1/scholarship_pages/data-science-apprenticeship-zeptolab'
+    )
+    if (!response.ok) {
+      throw new Error('Could not fetch the data')
     }
-    fetchDataFromApi()
-  }, [])
+    const data = await response.json()
+    console.log('data', data)
+    return data
+  } catch (e) {
+    console.log('Error fetching data:', e)
+    throw e
+  }
+}
+
+export default async function Home() {
+  const fetchedData = await fetchData()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-purple">Harbour.Space</div>
+      <div>
+        <FAQ faqs={fetchedData.scholarship.faqs} />
+      </div>
     </main>
   )
 }
