a60dc12402f47be9e8f489c5f5e709c0c2364832
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-05 14:58:07 +0200

    feat: faq template complete

--- components/FAQ.js
+++ components/FAQ.js
@@ -1,22 +1,34 @@
 'use client'
 
 export default function FAQ({ faqs }) {
-  console.log('faq', faqs.items)
   return (
-    <div className="flex ">
-      <div>
-        <h1>Frequently asked questions</h1>
+    <div>
+      <div className="flex">
+        <div>
+          <h1>Frequently asked questions</h1>
+        </div>
+        <div>
+          <p> Filter by:</p>
+          <select>
+            <option value="0">All</option>
+            {faqs.categories.map((category, index) => (
+              <option key={index} value={index + 1}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div>
-        <p> Filter by:</p>
-        <select>
-          <option value="0">All</option>
-          {faqs.categories.map((category, index) => (
-            <option key={index} value={index + 1}>
-              {category}
-            </option>
-          ))}
-        </select>
+        {faqs.items.map((item, index) => (
+          <div key={index}>
+            <p>{item.type}</p>
+            <p>{item.question}</p>
+            {item.answer.map((a, index) => (
+              <p key={index}>{a.data}</p>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
