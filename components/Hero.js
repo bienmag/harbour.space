a918386ee990eca284e66fd2649153da4a539f43
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-10 16:10:07 +0200

    feat: scholarship name

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,15 @@
 export default function Hero({ data }) {
-  return <div>Hero</div>
+  const Name = () => {
+    return (
+      <div>
+        <h1>{data.scholarship.name}</h1>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Name />
+    </div>
+  )
 }
