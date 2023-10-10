c4925b62b399dcf278d7f98ed42a80beba8fdab5
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-10 17:02:29 +0200

    feat: rounded picture and pattern

--- components/About.js
+++ components/About.js
@@ -1,3 +1,29 @@
-export default function About() {
-  return <div>About</div>
+import Image from 'next/image'
+
+export default function About({ about, photo }) {
+  const Picture = () => {
+    return (
+      <div className="h-80 w-80 rounded-full bg-pattern flex items-center justify-center">
+        <Image
+          src={photo}
+          alt="pattern"
+          width={250}
+          height={250}
+          style={{
+            borderRadius: '50%',
+            overflow: 'hidden',
+            width: 250,
+            height: 250,
+            objectFit: 'cover',
+          }}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Picture />
+    </div>
+  )
 }
