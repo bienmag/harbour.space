f188d0e6b44f9b100e7255b6e86645f378560495
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-10 15:43:52 +0200

    feat: header

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="z-20 fixed w-full bg-purple text-white h-12 flex items-center px-8">
+      HARBOUR.SPACE /Data Science
+    </div>
+  )
+}
