2873b148554032e113a3504c52f33c9bcdb8d1ed
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-10 16:15:45 +0200

    feat: company logo + next config

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Hero({ data }) {
   const Name = () => {
     return (
@@ -7,9 +9,27 @@ export default function Hero({ data }) {
     )
   }
 
+  const PoweredBy = () => {
+    return (
+      <div>
+        <div>
+          <p>Powered by: </p>
+          <p>&nbsp;Zeptolab</p>
+        </div>
+        <Image
+          src={data.scholarship.company.color_logo.rawFile.preview}
+          alt="company-logo"
+          width={80}
+          height={80}
+        ></Image>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Name />
+      <PoweredBy />
     </div>
   )
 }
